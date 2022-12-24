34c86064a08bd3b0349c1d444cda41559d94e08e
N0H3dy <workmail2776@gmail.com>
2022-12-24 12:28:03 -0600

    Created Login, and home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,70 +7,157 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Wrap(
-                spacing: 10,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('Money'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('Bitcoin'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('Stock Market'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('House Market'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('Dimond Hands'),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Wrap(
+                  spacing: 10,
                   children: [
-                    Image.asset('images/animated_ghost.gif'),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('Money'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('Bitcoin'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('Stock Market'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('House Market'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('Dimond Hands'),
+                    ),
                   ],
-                ))
-          ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return Container();
+                      },
+                    ),
+                  );
+                },
+                child: Container(
+                    width: double.infinity,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: Offset(
+                              2,
+                              2,
+                            ),
+                          ),
+                        ]),
+                    child: Column(
+                      children: [
+                        Image.asset('images/animated_ghost.gif'),
+                        const ListTile(
+                          title: Text('How to get rich'),
+                          trailing: Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                      ],
+                    )),
+              ),
+              Container(
+                  width: double.infinity,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(
+                            2,
+                            2,
+                          ),
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Image.asset('images/animated_ghost.gif'),
+                      const ListTile(
+                        title: Text('How to get rich'),
+                        trailing: Icon(Icons.arrow_forward_ios_rounded),
+                      ),
+                    ],
+                  )),
+              Container(
+                  width: double.infinity,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(
+                            2,
+                            2,
+                          ),
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Image.asset('images/animated_ghost.gif'),
+                      const ListTile(
+                        title: Text('How to get rich'),
+                        trailing: Icon(Icons.arrow_forward_ios_rounded),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
         ));
   }
 }
